2b011897b7a968635fd0e09e4bc07842d870016b
Peter Carl Pardo <peterpardo123@gmail.com>
2023-12-01 23:52:29 +0800

    Added middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,19 @@ Route::get('/blog', function () {
     return view('blog');
 });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', function () {
+        return view('auth.login');
+    })->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', function () {
+        return view('auth.register');
+    })->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
 });
